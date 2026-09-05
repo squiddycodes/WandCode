e1098fe4ac64bf91c4e766632476c7630680aae5
squiddycodes <dguidorizzi@yahoo.com>
2026-09-04 20:11:34 -0400

    ESP32 receiver & mock transmitter

---
+++ esp32_receiver.py
@@ -0,0 +1,30 @@
+import socket
+
+# Configure the listener to match the ESP32 settings
+UDP_IP = "0.0.0.0"  # Listens on all available network interfaces
+UDP_PORT = 5005 #change to be esp32's port
+
+# Create and bind the UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+print(f"ESP Listener active on port {UDP_PORT}...")
+
+try:
+    while True:
+        # Buffer size of 1024 bytes
+        data, addr = sock.recvfrom(1024)
+
+        ir_hex = data.decode("utf-8")
+        
+        print(f"Received IR Code: {ir_hex} from {addr[0]}")
+        
+        # ROOM CONTROL LOGIC - if A received from addr[A], send signal to turn on light
+        if ir_hex == "FF30CF":
+            #send signal to a 
+            print("Fireball!")
+
+except KeyboardInterrupt:
+    print("\nListener stopped.")
+finally:
+    sock.close()
